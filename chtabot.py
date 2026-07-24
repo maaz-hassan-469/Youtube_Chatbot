bbb71ac24c373dea4f0be7bc93cc35505405e348
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-24 02:42:37 -0700

    adding youtubeloader

--- chtabot.py
+++ chtabot.py
@@ -3,18 +3,26 @@ from langchain_huggingface import ChatHuggingFace,HuggingFaceEmbeddings,HuggingF
 from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled
 from langchain_chroma import Chroma
 from langchain_core.prompts import PromptTemplate
+from langchain_community.document_loaders import YoutubeLoader
 
 while(True):
     video_id=input("enter a video_id to ask question:")
     if video_id.lower()=="exit":
         break
+    formatted_url = f"https://www.youtube.com/watch?v={video_id}"
+    print(f"🔎 Fetching transcript for Video ID: '{video_id}'...")
     try:
-        transcript_list=YouTubeTranscriptApi.get_transcript(video_id,languages=["en"])
-        transcript=" ".join(chunk["text"] for chunk in transcript_list)
-        print("\n--- Transcript Retrieved Successfully ---")
-        print(transcript)
-    except:
-        print("no captions available for this video")
+        loader=YoutubeLoader.from_youtube_url(
+            formatted_url,
+            add_video_info=False,
+            language=["en","en-US","en-GB","hi"]
+        )
+
+        docs=loader.load()
+        print(docs[0].page_content[:400] + "...\n")
+
+    except Exception as e:
+        print(f"\nError Details: {e}")
 
 
         
